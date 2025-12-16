4508482398c7da23e45f7d6b04a9c84389a98561
정성찬 <jeonghaejun004@gmail.com>
2025-12-16 15:00:04 +0900

    feat: 타일 점수 적용
    타일에 점수 추가

--- Assets/3.Script/Tile/HexTile.cs
+++ Assets/3.Script/Tile/HexTile.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 public class HexTile : MonoBehaviour
 {
     public Vector2Int axialCoord;   // 이 타일의 육각 좌표
+    public int score;
+
+    private bool isStepped;
 
     // 플레이어가 밟았을 때 호출
     public virtual void OnStepped()
     {
+        if (isStepped)
+            return;
+
         TileManager.Instance.RequestTileCollapse(axialCoord);
+        GameManager.Instance.Score += score;
+        isStepped = true;
     }
 
 }
