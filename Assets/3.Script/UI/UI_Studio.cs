73d1fa49214c0e8687441fe254be6206e4c6e85f
magicJSC <jeonghaejun004@gmail.com>
2026-02-12 23:59:31 +0900

    fix: 불필요 코드 삭제

--- Assets/3.Script/UI/UI_Studio.cs
+++ Assets/3.Script/UI/UI_Studio.cs
@@ -1,17 +1,9 @@
-using GooglePlayGames;
+
 using UnityEngine;
 
 public class UI_Studio : MonoBehaviour
 {
     [SerializeField] UI_Loading loadingUI;
-
-    private void Start()
-    {
-        PlayGamesPlatform.Instance.Authenticate(result =>
-        {
-            Debug.Log("Login Result: " + result);
-        });
-    }
 
     public void StartLoading()
     {
